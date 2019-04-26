03b239e3a262d1eaf491e139590fdaf3d2cc03bc
yq_mc <573380218@qq.com>
2019-04-26 00:13:03 -0400

    Create rnn_train.py

---
+++ src/rnn_train.py
@@ -0,0 +1,17 @@
+"""
+tensorflow implementation of RNN
+"""
+import os
+import glob
+from argparse import ArgumentParser
+
+import numpy as np
+import tensorflow as tf
+
+# lib
+sys.path.append('../ext/')
+
+def train():
+
+
+

cab31466945ba1a8016d0193bb57b07416538d22
yq_mc <573380218@qq.com>
2019-04-26 05:07:15 -0400

    mc

---
+++ src/datagenerator.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+def train_data():
+
+
+def test_data():
+
+def data_split():
+
--- src/rnn_train.py
+++ src/rnn_train.py
@@ -9,9 +9,21 @@ import numpy as np
 import tensorflow as tf
 
 # lib
+import datagenerator
 sys.path.append('../ext/')
 
-def train():
+def train(iterations, load_iter, batch_size = 1):
 
 
 
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--load_iters", type=int,
+                        dest="load_iter", default=0,
+                        help="load iters")
+    parser.add_argument("--iters", type=int,
+                        dest="iterations", default=1000,
+                        help="number of iterations")
+    args = parser.parse_args()
+    train(**vars(args))

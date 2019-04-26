bdcb70f7ef0d7dacc7499daed943d27c4d0417f1
yq_mc <573380218@qq.com>
2019-04-26 13:36:13 -0400

    mc

--- src/datagenerator.py
+++ src/datagenerator.py
@@ -15,13 +15,14 @@ def demo_data(m = 30, n = 2, time_step = 15):
     data = np.zeros((m, n))
     for i in arange(n):
         data[0:int(m/2)-1, i] = np.random.uniform(low = 0.0, high = 5.0, size = (int(m/2), 1)) + i
-        data[int(m/2):m-1, i] = np.random.uniform(low = 0.0, high = 5.0, size = (int(m/2, 1)))
+        data[int(m/2):m-1, i] = math.cos() + i
     # generate positive
-    
+
     # generate negative
 
     # sliding window to generate data
 
+    return
 
 def data_split():
 

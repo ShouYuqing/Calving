80ba50dec935d547944d69141f537a6f03398885
yq_mc <573380218@qq.com>
2019-04-28 06:30:27 -0400

    mc

--- src/rnn_train.py
+++ src/rnn_train.py
@@ -93,7 +93,7 @@ def train(iterations, load_iter, batch_size = 20):
 
         # save model
         saver = tf.train.Saver()
-        saver.save(sess, "iter" + str(iteration))
+        saver.save(sess, "../models/iter" + str(iteration))
         # validation
         val_x, val_y = datagenerator.gene_batch(batch_size = 20, data = validate_input, label = validate_output)
         result = sess.run(predictions, feed_dict={x: val_x.reshape(val_x.shape[0], val_x.shape[1], len2*n), y_: val_y.reshape([-1, time_step]), keep_prob: 1.0})
